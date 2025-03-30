55c4ca04d61914211b6ebdbde87b633023d30ac1
programadoreduardo <programador.eduardo@gmail.com>
2025-03-30 10:19:00 -0300

    Removendo API key do código e usando variáveis de ambiente

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useAuth } from '../../context/authContext'
 
 export default function Home() {
+  const { logout } = useAuth()
+  const handleLogout = async () => {
+    await logout()
+  }
+
   return (
-    <View className= 'pt-20'>
-      <Text>home</Text>
-    </View>
+  <View className='pt-20'>
+    <Text>home</Text>
+    <Button title=' Sair' onPress={handleLogout} />
+  </View>
   )
 }
