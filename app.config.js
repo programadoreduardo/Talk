39bc47676c4b7c150103e28981d656af1fcd1ecf
programadoreduardo <programador.eduardo@gmail.com>
2025-03-30 11:08:52 -0300

    fix: remove sensitive keys from source code

---
+++ app.config.js
@@ -0,0 +1,46 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "talk",
+    slug: "talk",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      apiKey: process.env.GOOGLE_API_KEY
+    }
+  }
+};
