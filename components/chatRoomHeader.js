6b306428e01e2b1e43336ffa81c89abf48b78813
programadoreduardo <programador.eduardo@gmail.com>
2025-04-24 20:28:27 -0300

    configurando chat

--- components/chatRoomHeader.js
+++ components/chatRoomHeader.js
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Stack } from 'expo-router'
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, Ionicons } from '@expo/vector-icons'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { Image } from 'expo-image';
 
@@ -21,7 +21,14 @@ export default function ChatRoomHeader(user, router) {
                                 source={user?.profileUrl}
                                 style={{ height: hp(4.5), aspectRatio: 1, borderRadius: 100 }}
                             />
+                            <Text style={{ fontSize: hp(2.5) }} className='text-neutral-500 font-medium '
+                            >{user?.username}</Text>
                         </View>
+                    </View>
+                },
+                headerRight: () => {
+                    <View className='flex-row items-center gap-8'>
+                        <Ionicons name='call' size={hp(2.8)} color={'#737373'}/>
                     </View>
                 }
             }}
