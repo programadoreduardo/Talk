3d094c257ea224d6c4ff9eb2a0573eac9e6b4450
programadoreduardo <programador.eduardo@gmail.com>
2025-05-22 21:31:09 -0300

    finalizado o codigo do chat

--- components/MessageItem.js
+++ components/MessageItem.js
@@ -1,19 +1,30 @@
 import { View, Text } from "react-native";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 
 export default function MessageItem({ message, currentUser }) {
-    const isCurrentUser = currentUser?.userId === message?.userId;
+    if (currentUser?.userId === message?.userId) {
 
-    return (
-        <View style={{ 
-            alignSelf: isCurrentUser ? 'flex-end' : 'flex-start',
-            backgroundColor: isCurrentUser ? '#DCF8C6' : '#ECECEC',
-            padding: 10,
-            borderRadius: 10,
-            marginVertical: 5,
-            maxWidth: '80%',
-        }}>
-            <Text>{message?.message}</Text>
-        </View>
-    );
+        return (
+            <View className="flex-row justify-end mb-3 mr-2">
+                <View style={{ width: wp(80) }}>
+                    <View className="flex self-end p-3 rounded-2xl bg-white boder border-neutral-200">
+                        <Text style={{ fontSize: hp(1.9) }}>
+                            {message?.message}
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        );
+    } else {
+        return (
+            <View style={{ width: wp(80) }} className="mb-3 ml-3">
+                <View className="flex self-start p-3 px-4 rounded-2xl bg-indigo-100 border-indigo-200">
+                    <Text style={{ fontSize: hp(1.9) }}>
+                        {message?.message}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
 }
